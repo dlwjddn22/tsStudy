ae520075eb80edb588e41c7ae918dcb8c220573e
MNSOFT\O1067 <O1067@hyundaimnsoft.com>
2019-06-26 17:35:12 +0900

    2차 업로드

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 "use strict";
-console.log("typescript log");
+console.log("Typescript log1");
+var myName;
+myName = "이정우";
+//myName = 32;
+console.log(myName);
 var Color;
 (function (Color) {
     Color[Color["Blue"] = 0] = "Blue";
     Color[Color["Red"] = 1] = "Red";
     Color[Color["Yellow"] = 2] = "Yellow";
 })(Color || (Color = {}));
-console.log(Color.Red);
+;
+console.log(Color.Blue);
+function multiply(val1, val2) {
+    return val1 * val2;
+}
+console.log("multiply = " + multiply(3, 4));
+for (var i = 0; i < 9; i++) {
+    console.log(i + "번째");
+}
+function personInfoPrint(personInfo) {
+    console.log(personInfo.name);
+}
+var ps = {
+    name: "홍길동",
+    age: 20,
+    height: 180
+};
+personInfoPrint(ps);
--- main.ts
+++ main.ts
@@ -1,10 +1,46 @@
-console.log("typescript log");
+console.log("Typescript log1");
+
+let myName: String;
+myName = "이정우";
+//myName = 32;
+console.log(myName);
 
 enum Color
 {
     Blue,
     Red,
     Yellow
+};
+
+console.log(Color.Blue);
+
+function multiply(val1: number, val2: number): number
+{
+    return val1 * val2;
 }
 
-console.log(Color.Red);
+console.log("multiply = " + multiply(3, 4));
+
+for(let i=0; i < 9; i++)
+{
+    console.log(i + "번째");
+}
+
+interface Person{
+    name: string;
+    age: number;
+    height: number;
+}
+
+function personInfoPrint(personInfo: Person) : void {
+    console.log(personInfo.name);
+}
+
+
+let ps: Person = {
+    name: "홍길동",
+    age: 20,
+    height: 180
+}
+
+personInfoPrint(ps);
